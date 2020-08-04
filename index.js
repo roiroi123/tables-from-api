0dd0b581ccfbe68ebc826092bf0cb4f6bf8adb3d
Your Name <royshalom911@gmail.com>
2020-08-05 00:31:17 +0300

    finished

--- index.js
+++ index.js
@@ -1,105 +1,78 @@
-
 async function init() {
-    try {
-        const response = await getApi({url:`https://randomuser.me/api/?results=50`})
-        const { results } = response;
-        return results
-        
-        
-    } catch (error) {
-        alert("error")
-    }
+  try {
+    const response = await getApi({
+      url: `https://randomuser.me/api/?results=50`,
+    });
+    const { results } = response;
+    return results;
+  } catch (error) {
+    alert("error");
+  }
 }
 
-$("#btn1").one("click",async function(){
-    $("#table").html("")
-
-    const results = await init()
-  console.log(results);
-  const males =[]
-  const females =[]
-  const canada =[]
-  const Finland =[]
-  const mapped =results.map((e)=>{
-      if (e.gender === "male") {
-          males.push(e)
-      }
-      if (e.location.country === "Canada") {
-        canada.push(e)
+$("#btn1").on("click", async function () {
+  $("#table").html("");
+  const results = await init();
+  const males = [];
+  const females = [];
+  const genders = results.reduce((obj, current) => {
+    const { gender } = current;
+    if (gender === "male") {
+      males.push(gender);
+    } else {
+      females.push(gender);
     }
-    
-    if (e.location.country === "Finland") {
-      Finland.push(e)
-    }
-    if (e.gender === "female"){
-        females.push(e)
-
-      }
-  })
-  
-   drawHeader("male")
-   drawHeader("female")
-   drawTdGender(males.length,females.length)
-  
-  
-})
-$("#btn2").one("click",async function(){
-    $("#table").html("")
-    const results = await init()
-  console.log(results);
-  const allCountries = results.map(e=>{
-      return e.location.country
-    })
-    console.log(allCountries);
-    
-    const uniqueCountry = allCountries.reduce((unique,item)=>{
-       return unique.includes(item) ? unique :[...unique,item]
-    },[])
-
-   
-uniqueCountry.forEach(element => {
-    drawHeader(element)
-    drawTdCountry(getOccurrence(allCountries,element),"")
-    console.log(getOccurrence(allCountries,element)); 
-    
+  }, {});
+  drawHeader("male");
+  drawHeader("female");
+  drawTdGender(males.length, females.length);
 });
-   
-   
-  
-})
+$("#btn2").on("click", async function () {
+  $("#table").html("");
+  const results = await init();
+  const countries = results.reduce((obj, current) => {
+    const { country } = current.location;
+    const count = obj[country] || 0;
+    return { ...obj, [country]: count + 1 };
+  }, {});
+  const countryKeyVal = Object.entries(countries);
+  drawheaderCountry();
+  countryKeyVal.forEach((e) => {
+    const tbody = $(`<tbody id="tbody"></tbody>`);
+    const tdName = $(`<td>${e[0]} </td>`);
+    const tdAmount = $(`<td>${e[1]} users</td>`);
+    tbody.append(tdName, tdAmount);
+    $("#table").append(tbody);
+  });
+});
 
-
+function drawheaderCountry() {
+  const th1 = $(`<th scope="row">country</th>`);
+  const th2 = $(`<th scope="row">amount</th>`);
+  $("#table").append(th1, th2);
+}
 function drawHeader(th1name) {
-    const th1 = $(`<th scope="row">${th1name}</th>`)
-    
-    $("#table").append(th1)
-}
-function drawTdGender(td1,td2){
-    const tbody = $(`<tbody id="tbody"></tbody>`)
+  const th1 = $(`<th scope="row">${th1name}</th>`);
 
-        const tr = $(`<tr></tr>`)
-        const tdM = $(`<td>${td1} users</td>`)
-        const tdF = $(`<td>${td2} users</td>`)
-        tr.append(tdM,tdF)
-       tbody.append(tr)
-       $("#table").append(tbody)
-    ;
-    
+  $("#table").append(th1);
 }
-function drawTdCountry(td){
-    const tbody = $(`<tbody id="tbody"></tbody>`)
+function drawTdGender(td1, td2) {
+  const tbody = $(`<tbody id="tbody"></tbody>`);
 
-        const tr = $(`<tr></tr>`)
-        const tdC = $(`<td>${td} users</td>`)
-        
-        tr.append(tdC)
-       tbody.append(tr)
-       $("#table").append(tbody)
-    ;
-    
+  const tr = $(`<tr></tr>`);
+  const tdM = $(`<td>${td1} users</td>`);
+  const tdF = $(`<td>${td2} users</td>`);
+  tr.append(tdM, tdF);
+  tbody.append(tr);
+  $("#table").append(tbody);
 }
-function getOccurrence(array, value) {
-    var count = 0;
-    array.forEach((v) => (v === value && count++));
-    return count;
+function drawTdCountry(td) {
+  const tbody = $(`<tbody id="tbody"></tbody>`);
+
+  const tr = $(`<tr></tr>`);
+  const tdC = $(`<td>${td} users</td>`);
+
+  tr.append(tdC);
+  tbody.append(tr);
+  $("#table").append(tbody);
 }
